3372c3a8808f537be084697ecdf795c99d08801b
Juxsrck <73861576+Juxsrck@users.noreply.github.com>
2023-03-16 14:38:02 -0500

    Clase 18

---
+++ modules/src/main/java/com/platzi/functional/_04_functional/StringFunctions.java
@@ -0,0 +1,20 @@
+package com.platzi.functional._04_functional;
+
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+
+public class StringFunctions {
+    public static void main(String[] args) {
+        UnaryOperator<String> quote = text -> "\"" + text + "\"";
+        UnaryOperator<String> addMark = text -> text + "!";
+
+        System.out.println(quote.apply("Hola como vas tdbn?"));
+
+        System.out.println(addMark.apply("hola"));
+
+        BiFunction<String, Integer, String> leftPad =
+                (text, number) -> String.format("%" + number + "s", text);
+
+        System.out.println(leftPad.apply("JAva", 10));
+    }
+}
